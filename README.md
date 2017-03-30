02e3aefde73ea782e2594c5419643ab606212ce6
Philipp Fromme <philipp.fromme@camunda.com>
2017-03-30 16:40:16 +0200

    feat(project): support bpmn-js plugins

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-camunda-modeler-plugin-helpers
+# Camunda Modeler Plugin Helpers :electric_plug:
+
+Helper functions for bundling your Camunda Modeler client plugins.
+
+## How to use
+
+Your file to be bundled:
+
+```javascript
+var registerBpmnJSPlugin = require('./camundaModelerPluginHelpers').registerBpmnJSPlugin;
+var module = require('./index');
+
+registerBpmnJSPlugin(module);
+```
+
+## Currently Supported Client plugins
+
+- bpmn-js
---
+++ index.js
@@ -0,0 +1,41 @@
+/**
+ * Validate and register a client plugin.
+ *
+ * @param {Object} plugin
+ * @param {String} type
+ */
+function registerClientPlugin(plugin, type) {
+  var plugins = window.plugins || [];
+  window.plugins = plugins;
+
+  if (!plugin) {
+    throw new Error('plugin not specified');
+  }
+
+  if (!type) {
+    throw new Error('type not specified');
+  }
+
+  plugins.push({
+    plugin: plugin,
+    type: type
+  });
+}
+
+/**
+ * Validate and register a bpmn-js plugin.
+ *
+ * Example use:
+ *
+ *    var registerBpmnJSPlugin = require('./camundaModelerPluginHelpers').registerBpmnJSPlugin;
+ *    var module = require('./index');
+ *
+ *    registerBpmnJSPlugin(module);
+ *
+ * @param {Object} plugin
+ */
+function registerBpmnJSPlugin(plugin) {
+  registerClientPlugin(plugin, 'bpmn.modeler.additionalModules');
+}
+
+module.exports.registerBpmnJSPlugin = registerBpmnJSPlugin;
