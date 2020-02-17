a73bfd233f4a8e5d6ba120167b8f81b995ab3393
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-02-17 13:31:50 +0100

    docs(components): add examples and correct property name

--- components.js
+++ components.js
@@ -3,9 +3,24 @@ if (!window.components) {
 }
 
 /**
- * Fill component.
+ * Fill component. Set `slot` to "toolbar" to include in the top toolbar.
+ * Use `group` to place for correct ordering.
  *
- * @type {import('react').ComponentType<{ group: string, name: string }>}
+ * @type {import('react').ComponentType<{ slot: string, group?: string }>}
+ *
+ * @example
+ *
+ * import { Fill } from 'camunda-modeler-plugin-helpers/components';
+ *
+ * function CustomFill(props) {
+ *   return (
+ *     <Fill group="4_export" slot="toolbar">
+ *       <button type="button" onClick={ props.openExportTool }>
+ *         Open Export Tool
+ *       </button>
+ *     </Fill>
+ *   );
+ * }
  */
 export const Fill = window.components.Fill;
 
@@ -13,5 +28,27 @@ export const Fill = window.components.Fill;
  * Modal component.
  *
  * @type {import('react').ComponentType<{ onClose: Function }>}
+ *
+ * @example
+ *
+ * import { Modal } from 'camunda-modeler-plugin-helpers/components';
+ *
+ * function CustomModal(props) {
+ *   return (
+ *    <Modal onClose={ props.onClose }>
+ *      <Modal.Title>
+ *        Custom Modal
+ *      </Modal.Title>
+ *      <Modal.Body>
+ *        Hello world!
+ *      </Modal.Body>
+ *      <Modal.Footer>
+ *        <button type="button" onClick={ props.onClose }>
+ *          Close
+ *        </button>
+ *      </Modal.Footer>
+ *    </Modal>
+ *   );
+ * }
  */
 export const Modal = window.components.Modal;
