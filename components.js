3e47e637985939d1e9083ea80a6383045586f856
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2020-02-20 15:41:49 +0100

    docs(components): update Fill documentation

--- components.js
+++ components.js
@@ -4,9 +4,10 @@ if (!window.components) {
 
 /**
  * Fill component. Set `slot` to "toolbar" to include in the top toolbar.
- * Use `group` to place for correct ordering.
+ * Use `group` and `priority=0` to place for correct ordering. The higher
+ * the priority, the earlier the Fill is displayed within the group.
  *
- * @type {import('react').ComponentType<{ slot: string, group?: string }>}
+ * @type {import('react').ComponentType<{ slot: string, group?: string, priority?: Number }>}
  *
  * @example
  *
@@ -14,7 +15,7 @@ if (!window.components) {
  *
  * function CustomFill(props) {
  *   return (
- *     <Fill group="4_export" slot="toolbar">
+ *     <Fill group="4_export" slot="toolbar" priority={100}>
  *       <button type="button" onClick={ props.openExportTool }>
  *         Open Export Tool
  *       </button>
